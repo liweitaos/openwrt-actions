18be2ae27e14df22b2948411c1aa39a36967b57f
liweitaos <48864888+liweitaos@users.noreply.github.com>
2023-02-23 21:05:07 +0800

    调整构建配置

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,5 @@
 
 sed -i 's/#src-git helloworld/src-git helloworld/g' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i 's|https://github|https://gitclone.com/github|g' feeds.conf.default
